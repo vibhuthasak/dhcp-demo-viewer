237cbd63845a9b3fa2fe691d96f525bb2d65d025
kavindi <kavindiv1996@gmail.com>
2020-11-26 16:34:58 +0530

    arrow direction added to server

--- src/server/emitters.js
+++ src/server/emitters.js
@@ -1,35 +1,45 @@
 export const watchDHCP = (socket, client) => {
     client.on('message', function (data) {
         let DHCPCode = "UNKNOWN"
+        let arrowDirection = ""
         switch(data.options[53]) {
             case 1:
                 DHCPCode = "DHCP DISCOVER"
+                arrowDirection = "right"
                 break
             case 2:
                 DHCPCode = "DHCP OFFER"
+                arrowDirection = "left"
                 break
             case 3:
                 DHCPCode = "DHCP REQUEST"
+                arrowDirection = "right"
                 break
             case 4:
                 DHCPCode = "DHCP DECLINE"
+                arrowDirection = "left"
                 break
             case 5:
                 DHCPCode = "DHC PACK"
+                arrowDirection = "left"
                 break
             case 6:
                 DHCPCode = "DHCP NAK"
+                arrowDirection = "left"
                 break
             case 7:
                 DHCPCode = "DHCP RELEASE"
+                arrowDirection = "right"
                 break
             case 8:
                 DHCPCode = "DHCP INFORM"
+                arrowDirection = "right"
                 break
         }
         socket.emit("DHCP_STATUS", {
             ...data,
-            DHCPCode
+            DHCPCode,
+            arrowDirection
         });
     });
 }
